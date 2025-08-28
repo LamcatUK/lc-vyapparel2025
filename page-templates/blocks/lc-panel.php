9aa0211f4a46184da4973d6761c461f6be56b49b
LamcatUK <git@lamcat.co.uk>
2025-08-28 18:54:20 +0100

    merch panel

--- page-templates/blocks/lc-panel.php
+++ page-templates/blocks/lc-panel.php
@@ -229,7 +229,7 @@ $variant_data = is_string( $variant ) ? get_field( $variant ) : null;
                     if ( 'divider' === $layout ) {
                         ?>
             <h2 class="u-headline text-uppercase text-black boxed mb-5">
-                        <?= wp_kses_post( get_sub_field( 'text' ) ); ?>
+                        <?= wp_kses_post( get_sub_field( 'divider' ) ); ?>
             </h2>
                         <?php
                     }
